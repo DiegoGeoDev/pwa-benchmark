84945448b9962a11ec522c499dea5b6d974a5a15
diego.geodev <diego.geodev@gmail.com>
2026-02-07 18:40:20 -0300

    feat: add Angular CDK dependency and update home page layout with navigation button

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { ZardButtonComponent } from '@/shared/components/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ZardButtonComponent, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-
-}
+export class Home {}
